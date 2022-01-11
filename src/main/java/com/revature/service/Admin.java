f5ba56efbd922fbd6002f17868efe3bd92e35025
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-01-10 21:32:21 -0800

    created classes for admin and manager with constructors that extend super. Necessary variables for employee info

---
+++ src/main/java/com/revature/service/Admin.java
@@ -0,0 +1,11 @@
+package com.revature.service;
+
+import java.util.ArrayDeque;
+import java.util.List;
+
+public class Admin extends User{
+    //constructor matching Super
+    public Admin(int id, String firstName, String lastName, String fullName, int dept, List<Integer> hoursWorked, String manager, ArrayDeque<String> employees) {
+        super(id, firstName, lastName, fullName, dept, hoursWorked, manager, employees);
+    }
+}
