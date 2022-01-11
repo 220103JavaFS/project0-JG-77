8d0166414fcce9d31a44e632739bb74b4bbbbb26
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-01-10 21:38:31 -0800

    added logback.xml file in resources folder. Ran main() to see if log.txt file would generate properly. logback functionality disabled for now

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -1,7 +1,10 @@
 package com.revature;
 
-public class Driver {
-    public static void main(String[] args) {
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
+public class Driver {
+   // private static Logger log = LoggerFactory.getLogger(Driver.class);
+    public static void main(String[] args) {
     }
 }
