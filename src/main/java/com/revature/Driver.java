c73c5c9ad1bbdb8292eda9bb32532be6bff8be11
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-01-11 19:12:05 -0800

    set up Javalin in Driver class, and set up main Controller class inside Controller layer.

--- src/main/java/com/revature/Driver.java
+++ src/main/java/com/revature/Driver.java
@@ -3,8 +3,16 @@ package com.revature;
 //import org.slf4j.Logger;
 //import org.slf4j.LoggerFactory;
 
+import com.revature.controller.Controller;
+import io.javalin.Javalin;
+
 public class Driver {
+    private static Javalin app;
    // private static Logger log = LoggerFactory.getLogger(Driver.class);
     public static void main(String[] args) {
+    }
+
+    public static void configure(Controller... controllers){
+
     }
 }
