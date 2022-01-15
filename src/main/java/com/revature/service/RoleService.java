c8e6eb0de4d8ea25db7632f1c59da490d8203cb0
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-01-15 13:07:29 -0800

    set up first method in service layer connecting to the DAO

--- src/main/java/com/revature/service/RoleService.java
+++ src/main/java/com/revature/service/RoleService.java
@@ -1,4 +1,17 @@
 package com.revature.service;
 
+import com.revature.models.Roles;
+import com.revature.dao.RoleDAO;
+import com.revature.dao.RoleDAOImp;
+
+import java.util.List;
+
 public class RoleService {
+
+    private RoleDAO roleDAO = new RoleDAOImp(); //call interface RoleDAO using RoleDAOImp class implementation
+
+    public List<Roles> findAll(){
+        return roleDAO.findAllRoles();
+    }
+
 }
