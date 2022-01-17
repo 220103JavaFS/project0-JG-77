66ecf8d1844c192151dce1a92312a8e27d4e520b
Jessie Guadarrama <jesguadarrama98@gmail.com>
2022-01-17 15:14:26 -0800

    Set up EmployeeService and implemented employee controller class

--- src/main/java/com/revature/controller/EmployeeController.java
+++ src/main/java/com/revature/controller/EmployeeController.java
@@ -1,11 +1,17 @@
 package com.revature.controller;
 
+import io.javalin.Javalin;
 import io.javalin.http.Handler;
 
-public class EmployeeController {
+public class EmployeeController implements Controller{
 
     //Handler is functional interface
     private Handler seeTheManager = (ctx) -> { //employee can see their manager
         //will make a call to Database
     };
+
+    @Override
+    public void addRoutes(Javalin app) {
+
+    }
 }
--- src/main/java/com/revature/service/EmployeeService.java
+++ src/main/java/com/revature/service/EmployeeService.java
@@ -1,9 +1,33 @@
 package com.revature.service;
 
+import com.revature.dao.EmployeeDAO;
+import com.revature.dao.EmployeeDAOImp;
 import com.revature.models.Employee;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class EmployeeService {
 
+    EmployeeDAO employeeDAO = new EmployeeDAOImp();
+
+    public List<Employee> findAllEmployees(){
+        return employeeDAO.findAllEmp();
+    }
+
+    public Employee findEmployee(int id){
+        return employeeDAO.findEmp(id);
+    }
+
+    public boolean addEmployee(Employee employee){
+        return employeeDAO.addEmp(employee);
+    }
+
+    public boolean updateEmployee(Employee employee){
+        return employeeDAO.updateEmp(employee);
+    }
+
+    public boolean terminateEmployee(int id){
+        return employeeDAO.terminateEmp(id);
+    }
 }
